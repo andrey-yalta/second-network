1a95b4c81a183942852f1f8991a61efe5799e6f3
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-15 07:21:15 +0300

    create grid css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
+import icon from "./vk.png"
+import profileBackground from "./profile.jpeg"
 
 
 
 const App =()=>{
   return(
-      <div>
-          <Header/>
-        <h1>Hello world!</h1>
+      <div className={"main"}>
+         <header>
+             <img src={icon} alt="network"/>
+         </header>
+          <div className={"sidebar"} >
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Message</a></li>
+                    <li><a href="#">Users</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+          </div>
+          <div className={"content"} >
+              <img src={profileBackground} alt="prfile"/>
+          </div>
       </div>
   )
 
