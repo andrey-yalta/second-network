ab2135d38c2df8a27460f64e65b9dfa6930b700c
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-15 06:30:35 +0300

    9 lesson create separate file header import export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import style from "./Header.module.css"
+import Header from "./Header";
+
 
 
 const App =()=>{
@@ -14,17 +15,3 @@ const App =()=>{
 }
 
 export default App;
-const Header = ()=>{
-    return(
-        <div >
-            <nav className={style.navigate}>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li> <a href="#">Message</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Settings</a></li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
