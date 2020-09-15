10d64be21373a1d0ad6e6c2c7769b6f34e074f03
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-15 17:03:25 +0300

    14 lesson css modules

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,16 @@ import React from "react";
 import s from "./Navbar.module.css"
 
 const Navbar =()=> {
+
     return(
         <div className={s.sidebar} >
             <ul>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Message</a></li>
-                <li><a href="#">Users</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li>
-                <li><a href="#">Settings</a></li>
+                <li><a className={s.item} href="#">Profile</a></li>
+                <li><a className={s.item} href="#">Message</a></li>
+                <li><a className={s.item} href="#">Users</a></li>
+                <li><a className={s.item} href="#">News</a></li>
+                <li><a className={s.item} href="#">Music</a></li>
+                <li><a className={s.item} href="#">Settings</a></li>
             </ul>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+    // Здесь важный момент, мы вставляем нашу главную компоненту из App
+    // в Div с классом root. т.е. root у нас единственный и вызывается он в index html
+
+
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
