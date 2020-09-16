8769ed6c2f0ff4c68c6ccb70446cee43f16ad961
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-16 15:39:09 +0300

    lesson 22 route exact and navlink again for  dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,14 +1,15 @@
 import React from "react";
 import s from "./Dialogs.module.css"
 import userIcon from "../../common/img/user.png"
+import {NavLink} from "react-router-dom";
 const Dialogs =()=>{
     return(
         <div className={s.dialogs}>
             <div className={s.users}>
-                <div className={`${s.user} ${s.active}`}><img src={userIcon} alt=""/> Ivan</div>
-                <div className={s.user}><img src={userIcon} alt=""/> Sveta</div>
-                <div className={s.user}><img src={userIcon} alt=""/> Sasha</div>
-                <div className={s.user}><img src={userIcon} alt=""/> Igor</div>
+                <div className={`${s.user} ${s.active}`}><img src={userIcon} alt=""/> <NavLink to={"/dialogs/1"}>Ivan</NavLink> </div>
+                <div className={s.user}><img src={userIcon} alt=""/><NavLink to={"/dialogs/2"}> Sveta</NavLink></div>
+                <div className={s.user}><img src={userIcon} alt=""/><NavLink to={"/dialogs/3"}> Sasha</NavLink></div>
+                <div className={s.user}><img src={userIcon} alt=""/><NavLink to={"/dialogs/4"}> Igor</NavLink></div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hello</div>
