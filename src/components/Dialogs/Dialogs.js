21e103e90a9883d343dd3b043d231b91f6a18aaf
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-16 15:25:51 +0300

    lesson 21 createdialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,9 +1,22 @@
 import React from "react";
 import s from "./Dialogs.module.css"
+import userIcon from "../../common/img/user.png"
 const Dialogs =()=>{
     return(
         <div className={s.dialogs}>
-            Dialogs
+            <div className={s.users}>
+                <div className={`${s.user} ${s.active}`}><img src={userIcon} alt=""/> Ivan</div>
+                <div className={s.user}><img src={userIcon} alt=""/> Sveta</div>
+                <div className={s.user}><img src={userIcon} alt=""/> Sasha</div>
+                <div className={s.user}><img src={userIcon} alt=""/> Igor</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>I'm fucking programmer</div>
+                <div className={s.message}>really?</div>
+            </div>
         </div>
     )
 }
