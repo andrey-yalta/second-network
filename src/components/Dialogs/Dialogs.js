010fe9c3238c64a963757ce71bb14bc09a7723bf
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-09-17 06:32:57 +0300

    ui bll lesson 24

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -16,23 +16,38 @@ const Message = (props) =>{
     )
 }
 
+let usersData = [
+    {id:1,name:"Dimych"},
+    {id:2,name:"Ivan"},
+    {id:3,name:"Sveta"},
+    {id:4,name:"Olga"},
+    {id:5,name:"Sasha"},
+   ]
+let dialogsData = [
+    {id:1,message:"hello"},
+    {id:2,message:"WHats up?"},
+    {id:3,message:"How is your name?"},
+    {id:4,message:"bla bla bla"},
+    {id:5,message:"Yo"},
+    {id:6,message:"Hello from Yata with love"},
+    ]
+
 const Dialogs =()=>{
     return(
         <div className={s.dialogs}>
             <div className={s.users}>
-                <UserItem id={1} name={"Dimych"}/>
-                <UserItem id={2} name={"Ivan"}/>
-                <UserItem id={3} name={"Sveta"}/>
-                <UserItem id={4} name={"Sasha"}/>
-
+                <UserItem id={usersData[0].id} name={usersData[0].name}/>
+                <UserItem id={usersData[1].id} name={usersData[1].name}/>
+                <UserItem id={usersData[2].id} name={usersData[2].name}/>
+                <UserItem id={usersData[3].id} name={usersData[3].name}/>
             </div>
             <div className={s.messages}>
-                <Message message={"hello"}/>
-                <Message message={"WHats up?"}/>
-                <Message message={"How is your name?"}/>
-                <Message message={"bla bla bla"}/>
-                <Message message={"Yo"}/>
-                <Message message={"Hello from Yata with love"}/>
+                <Message message={dialogsData[0].message} id={dialogsData[0].id}/>
+                <Message message={dialogsData[1].message} id={dialogsData[1].id}/>
+                <Message message={dialogsData[2].message} id={dialogsData[2].id}/>
+                <Message message={dialogsData[3].message} id={dialogsData[3].id}/>
+                <Message message={dialogsData[4].message} id={dialogsData[4].id}/>
+
             </div>
         </div>
     )
