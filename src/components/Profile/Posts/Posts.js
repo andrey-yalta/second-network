8bdaed55f68f56ddd1fff42617ba0f90757be97c
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-10-08 06:19:24 +0300

    lesson 87 PureCOmponent and React.memo

--- src/components/Profile/Posts/Posts.js
+++ src/components/Profile/Posts/Posts.js
@@ -1,18 +1,20 @@
 import React from "react";
-import s from "./Posts.module.css"
 import PostReduxForm from "./PostForm";
 
-const Posts =(props)=>{
-    let addPost =(state)=>{
+// функция memo работает примерно как PureComponent в классовых компонентах т.е она содержит свой
+// shouldComponentUpdate и она проверяет какой приходит state и props и если они не отлчаются от старых
+//то перерендера страницы не получается и как следствие - приложение работает быстрее и расходует меньше памяти
+const  Posts = React.memo(props=> {
+    let addPost = (state) => {
         props.addPost(state.post);
     }
-    return(
+    return (
         <div>
             <PostReduxForm onSubmit={addPost}/>
             {props.postsElements}
         </div>
     )
-}
+});
 export default Posts;
 
 
